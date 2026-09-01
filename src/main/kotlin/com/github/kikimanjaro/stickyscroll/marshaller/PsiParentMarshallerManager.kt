406d4f73c58a7dd694695c36ccbfb50b5d02b21c
KikiManjaro <kylian.meulin@gmail.com>
2026-09-01 07:29:37 +0200

    Add JavaScript support
    Fixes #7
    
    - Add JavaScriptParentMarshaller filtering classes, functions and methods via runtime heuristics
    - Register marshaller for Language JavaScript/ECMAScript 6 in PsiParentMarshallerManager
    - Add optional dependency on JavaScript plugin via withJS.xml

--- src/main/kotlin/com/github/kikimanjaro/stickyscroll/marshaller/PsiParentMarshallerManager.kt
+++ src/main/kotlin/com/github/kikimanjaro/stickyscroll/marshaller/PsiParentMarshallerManager.kt
@@ -9,6 +9,7 @@ class PsiParentMarshallerManager {
         private val jsonParentMarshaller = JsonParentMarshaller()
         private val xmlParentMarshaller = XMLParentMarshaller()
         private val pythonParentMarshaller = PythonParentMarshaller()
+        private val javaScriptParentMarshaller = JavaScriptParentMarshaller()
         fun getParentMarshaller(language: Language?): PsiParentMarshaller? {
             if (language == Language.findLanguageByID("kotlin")) {
                 return kotlinParentMarshaller
@@ -18,6 +19,10 @@ class PsiParentMarshallerManager {
                 return xmlParentMarshaller
             } else if (language == Language.findLanguageByID("Python")) {
                 return pythonParentMarshaller
+            } else if (language == Language.findLanguageByID("JavaScript") || language == Language.findLanguageByID("ECMAScript 6")) {
+                return javaScriptParentMarshaller
+            } else if (language?.id == "JavaScript" || language?.id == "ECMAScript 6" || language?.id == "JavaScript.reload") {
+                return javaScriptParentMarshaller
             } else {
                 return defaultParentMarshaller
             }
