6c746ca935684711d89b76cfcbce16286f7d9548
KikiManjaro <kylian.meulin@gmail.com>
2026-09-01 07:29:41 +0200

    Add PHP support
    Fixes #1
    
    - Add PhpParentMarshaller supporting classes, interfaces, traits, functions, methods and namespaces via runtime heuristics
    - Register marshaller for Language PHP in PsiParentMarshallerManager
    - Add optional dependency on PHP plugin (com.jetbrains.php) via withPHP.xml

--- src/main/kotlin/com/github/kikimanjaro/stickyscroll/marshaller/PsiParentMarshallerManager.kt
+++ src/main/kotlin/com/github/kikimanjaro/stickyscroll/marshaller/PsiParentMarshallerManager.kt
@@ -9,6 +9,7 @@ class PsiParentMarshallerManager {
         private val jsonParentMarshaller = JsonParentMarshaller()
         private val xmlParentMarshaller = XMLParentMarshaller()
         private val pythonParentMarshaller = PythonParentMarshaller()
+        private val phpParentMarshaller = PhpParentMarshaller()
         fun getParentMarshaller(language: Language?): PsiParentMarshaller? {
             if (language == Language.findLanguageByID("kotlin")) {
                 return kotlinParentMarshaller
@@ -18,6 +19,10 @@ class PsiParentMarshallerManager {
                 return xmlParentMarshaller
             } else if (language == Language.findLanguageByID("Python")) {
                 return pythonParentMarshaller
+            } else if (language == Language.findLanguageByID("PHP")) {
+                return phpParentMarshaller
+            } else if (language?.id == "PHP") {
+                return phpParentMarshaller
             } else {
                 return defaultParentMarshaller
             }
